e75750f01bd47e6f257011c119d595778747ca5e
Jenisha Bhattacharya <jenishabhattacharya@gmail.com>
2026-07-02 21:54:35 +0530

    added paths and modified style

--- bridge/urls.py
+++ bridge/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name = 'index')
-
+    path('', views.index,name = 'index'),
+    path('donate',views.donate,name = 'donate'),
+    path('nearby/', views.nearby, name='nearby'),
 ]
+
--- bridge/views.py
+++ bridge/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
-
-
+def donate(request):
+    return render(request,'donate.html')
+def nearby(request):
+    return render(request, 'nearby.html')
