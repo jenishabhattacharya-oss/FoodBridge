f6d34b36d7493c49d7076147a2cf5eca3eee4b49
Sourya Roy <souryaroy30@gmail.com>
2026-07-04 17:18:15 +0530

    feat(auth): initialize custom authentication system

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from .managers import UserManager
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
     class Role(models.TextChoices):
         DONOR = "DONOR", "Donor"
         VOLUNTEER = "VOLUNTEER", "Volunteer"
         NGO = "NGO", "NGO"
 
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15)
     role = models.CharField(
         max_length=20,
         choices=Role.choices,
     )
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+    REQUIRED_FIELDS = [
+        "first_name",
+        "last_name",
+        "phone",
+        "role",
+    ]
