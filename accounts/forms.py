d5b9b58070a24c1dad073369647b62cace5d0326
Sourya Roy <souryaroy30@gmail.com>
2026-07-10 20:38:03 +0530

    feat(accounts): add 'forms.py' create the base registration form of user

---
+++ accounts/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from .models import User
+
+
+class BaseUserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "first_name",
+            "last_name",
+            "phone",
+        )
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords don't match")
+
+        return cleaned_data
+
+    def _create_user(self, role):
+        data = self.cleaned_data
+
+        user = User.objects.create_user(
+            email=data["email"],
+            first_name=data["first_name"],
+            last_name=data["last_name"],
+            phone=data["phone"],
+            password=data["password"],
+            role=role,
+        )
+
+        return user
