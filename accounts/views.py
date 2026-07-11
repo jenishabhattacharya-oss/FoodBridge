a3fe55c898fbdfc893af7583cb9ad249cfd17745
Sourya Roy <souryaroy30@gmail.com>
2026-07-11 14:28:43 +0530

    refactor(accounts): centralize registration and remove donor-specific registration route

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from donors.forms import DonorRegistrationForm
+from accounts.models import User
+
 
 # Create your views here.
+def register(request):
+    pass
--- donors/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "donors"
-
-urlpatterns = [
-    path("register/", views.donor_register, name="register"),
-]
